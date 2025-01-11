b27519cf6a72bcae4106ec2a74976baf97c78dc1
Shardly Romelus <rshardlypicard89@gmail.com>
2025-01-10 20:10:16 -0500

    add controller address validation to controller

--- app/controllers/weather_forecasts_controller.rb
+++ app/controllers/weather_forecasts_controller.rb
@@ -1,53 +1,47 @@
 class WeatherForecastsController < ApplicationController
-  def index
-    @weather_forecasts = WeatherForecast.all
-  end
+  before_action :set_weather_forecast, only: [:index, :search]
 
-  def show
-    # debugger
+  def index
   end
 
   def search
-    if params[:address].present?
-      results = Geocoder.search(params[:address])
+    result = WeatherLookupService.new(Address.new(params[:address])).perform
+
+    if address.valid?
+      results = Geocoder.search(address.formatted_address)
       if location = results.first
         @latitude = location.latitude
         @longitude = location.longitude
         @zipcode = location.postal_code
 
+        @cache_hit = Rails.cache.exist?("weather_forecast_#{@zipcode}")
 
-        zip_code = ZipCode.find_or_create_by(code: @zipcode)
-        weather_service = WeatherService.new
-        forecast_data = weather_service.get_forecast(@latitude, @longitude)
+        @weather_forecast = Rails.cache.fetch("weather_forecast_#{@zipcode}", expires_in: 10.seconds) do
+          zip_code = ZipCode.find_or_create_by(code: @zipcode)
+          weather_service = WeatherService.new
+          forecast_data = weather_service.get_forecast(@latitude, @longitude)
 
-        @weather_forecasts = if forecast_data
-          WeatherForecast.create_or_update_from_api(forecast_data, zip_code)
-        else
-          []
+          if forecast_data
+            WeatherForecast.create_or_update_from_api(forecast_data, zip_code)
+          else
+            []
+          end
         end
       else
         flash.now[:alert] = "Could not find location for that address"
-        @weather_forecasts = []
       end
     else
-      @weather_forecasts = WeatherForecast.all
+      flash.now[:alert] = address.errors.full_messages.join(', ')
     end
 
     render :index
   end
 
-  def new
-  end
+  private
 
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+  def set_weather_forecast
+    @weather_forecasts = Rails.cache.fetch("weather_forecasts", expires_in: 5.seconds) do
+      WeatherForecast.limit(3).includes(:zip_code).order(created_at: :desc)
+    end
   end
 end
--- app/models/weather_forecast.rb
+++ app/models/weather_forecast.rb
@@ -4,6 +4,30 @@ class WeatherForecast < ApplicationRecord
   validates :zip_code_id, presence: true
   validates :current_temp, presence: true, numericality: true
   validates :date, presence: true
+  validates :condition_icon, presence: true
   validates :high_temp, :low_temp, numericality: { decimal: true }, allow_nil: true
+
+  def self.create_or_update_from_api(forecast_data, zip_code)
+    zip_code.update!(
+      city: forecast_data[:city],
+      state: forecast_data[:state],
+      latitude: forecast_data[:latitude],
+      longitude: forecast_data[:longitude]
+    )
+
+    forecast = WeatherForecast.new(zip_code: zip_code, date: Date.today)
+
+    forecast.tap do |f|
+      f.current_temp = forecast_data[:current_temp]
+      f.high_temp = forecast_data[:high_temp]
+      f.low_temp = forecast_data[:low_temp]
+      f.condition = forecast_data[:condition]
+      f.condition_icon = forecast_data[:condition_icon]
+      f.feels_like = forecast_data[:feels_like]
+      f.wind_speed = forecast_data[:wind_speed]
+      f.last_updated = forecast_data[:last_updated]
+      f.save!
+    end
+  end
 end
 
